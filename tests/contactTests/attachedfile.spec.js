bc1b27978c91eeda677626294281f32938c525b0
MihaiSimescu <mihai.simescu@emeal.nttdata.com>
2025-05-19 14:00:50 +0300

    Added stept to send a message.

--- tests/contactTests/attachedfile.spec.js
+++ tests/contactTests/attachedfile.spec.js
@@ -10,7 +10,14 @@ let messageId;
 let bodyLoginAdmin ={
     email: "admin@practicesoftwaretesting.com",
     password: "welcome01"   
-}  
+} 
+
+const contactBody = {
+    name : "John Doe",
+    email : "john@doe.example",
+    subject: "website",
+    message: "Something is wrong with the website." 
+ }
 
 
 test.beforeAll(async () => {
@@ -20,8 +27,28 @@ test.beforeAll(async () => {
 })
 
 test('Attach file to contact message', async ({ }) => {
- 
-    await test.step('1. Login as admin', async() => {
+    
+    await test.step('1. Send new contact message', async() => {
+
+        const contactRequest = await apiContext.post(contactURL, {
+                data: contactBody
+            })
+        
+            const jsonContactRequest = await contactRequest.json()
+        
+            expect(contactRequest.ok()).toBeTruthy()
+            expect(jsonContactRequest.name).toEqual(contactBody.name)
+            expect(jsonContactRequest.email).toEqual(contactBody.email)
+            expect(jsonContactRequest.subject).toEqual(contactBody.subject)
+            expect(jsonContactRequest.message).toEqual(contactBody.message)
+            expect(jsonContactRequest.status).toEqual('NEW')
+            expect(jsonContactRequest.id).toEqual(expect.any(String))
+
+
+    })
+
+
+    await test.step('2. Login as admin', async() => {
         const loginRequest = await apiContext.post(loginURL, 
            { data: bodyLoginAdmin }
         )
@@ -39,7 +66,7 @@ test('Attach file to contact message', async ({ }) => {
         access_token = jsonLoginRequest.access_token
     })
 
-    await test.step('2. Get messages', async() => {
+    await test.step('3. Get messages', async() => {
         const messagesRequest = await apiContext.get(contactURL,
             { 
                 headers: {
@@ -57,7 +84,7 @@ test('Attach file to contact message', async ({ }) => {
 
     })
 
-    await test.step('3. Attach file to contact message', async() => {
+    await test.step('4. Attach file to contact message', async() => {
         // Prepare the file to attach
     const attachFileURL = `/messages/${messageId}/attach-file`;
     const filePath = 'c:/Training/Proiecte_Playwright/ToolShop/tests/sample.txt';
